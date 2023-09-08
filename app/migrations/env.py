fa76f2f4705540dc0dfd250b9815de30b02b61f4
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-09-08 10:08:08 +0500

    1. Знакомство с фр. 15. База Данных Подключение

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -12,6 +12,7 @@ sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from app.database import Base, DATABASE_URL
 from app.hotels.models import Hotels
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
