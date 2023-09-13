d19b1340023932fe64951e9e22099562a812f200
Ñartyom-vah <art.vakhrushev@gmail.com>
2023-09-13 14:25:23 +0500

    1-6-8

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,8 +7,24 @@ class BaseDAO:
     model = None
 
     @classmethod
-    async def find_all(cls):
+    async def find_by_id(cls, model_id: int):
         async with async_session_maker() as session:
-            query = select(cls.model)  # SELECT * FROM bookings
+            query = select(cls.model).filter_by(id=model_id)
+            res = await session.execute(query)
+            return res.scalar_one_or_none()
+
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            res = await session.execute(query)
+            return res.scalar_one_or_none()
+
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
             res = await session.execute(query)
             return res.scalars().all()
